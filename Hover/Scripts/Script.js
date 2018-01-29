87144310f2381f871138aa8c1d2e3e715bc637f6
Herman Gouw <hermangouw@gmail.com>
2018-01-29 14:37:42 +1100

    Initial Version

---
+++ Hover/Scripts/Script.js
@@ -0,0 +1,16 @@
+﻿var clickMe = document.getElementById('click-me');
+clickMe.onclick = function () {
+    if (this.style.backgroundColor) {
+        this.style.backgroundColor = '';
+    } else {
+        this.style.backgroundColor = 'red';
+    }
+};
+
+d3.selectAll('.hover-me')
+    .on('mouseover', function () {
+        this.style.backgroundColor = 'yellow';
+    })
+    .on('mouseleave', function () {
+        this.style.backgroundColor = '';
+    });
